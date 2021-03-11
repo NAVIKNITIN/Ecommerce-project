767bacdd5034e7b2fc2c509a8688fe516fa91524
NavikNitin <naviknitin143@gmail.com>
2021-03-11 18:13:26 +0530

    --- Auto-Git Commit ---

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -2,6 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 const { getProducts, newProduct } = require('../controllers/productController');
-router.route("/products").get(getProducts);
-router.route("/product/new").post(newProduct);
+router
+    .route("/products")
+    .get(getProducts);
+router
+    .route("/product/new")
+    .post(newProduct);
 module.exports = router;
